a5ee3f0517d1091c05c1d51b605a73dd882ed6bc
Bobi Basari <bobibasari@gmail.com>
2020-03-29 19:50:15 +0700

    e-kantin-unpar-backend

---
+++ controllers/adminControllers.js
@@ -0,0 +1,37 @@
+// Import Database
+const db = require('../database/db');
+
+module.exports = {
+	/**
+	 * @routes POST admin/login
+	 * @description Admin login action
+	 * @access Admin
+	 */
+	adminLogin: (req, res) => {
+		// Get email & password
+		const { adminMail, adminPassword } = req.body.data;
+
+		// Validation email and password
+		if (adminMail === undefined || adminPassword === undefined) {
+			return res
+				.status(200)
+				.send({ error: true, message: 'Kolom email/password tidak boleh kosong!' });
+		} else {
+			// Set SQL Syntax
+			const sqlLogin = 'SELECT * FROM admin WHERE adminMail = ? AND adminPassword = ?';
+
+			// Database Action
+			db.query(sqlLogin, [adminMail, adminPassword], (err, loginResult) => {
+				if (err) res.status(200).send(err);
+
+				if (loginResult.length === 0) {
+					return res
+						.status(200)
+						.send({ error: true, message: 'Email yang anda masukkan tidak terdaftar!' });
+				} else {
+					return res.status(200).send({ error: false, result: loginResult });
+				}
+			});
+		}
+	}
+};
---
+++ controllers/publicControllers.js
@@ -0,0 +1,64 @@
+// Import Database
+const db = require('../database/db');
+
+module.exports = {
+	/**
+	 * @routes POST public/stand
+	 * @description Public get list stand
+	 * @access Public
+	 */
+	publicGetListStand: (req, res) => {
+		// Set SQL Syntax
+		const sql = `
+      SELECT 
+        sp.standName,
+        sp.standContact,
+        sp.standPhoto,
+        sa.standAddress
+      FROM
+        stand_profile sp
+          JOIN
+            stand_address sa ON sa.profileId = sp.profileId`;
+
+		// Database Action
+		db.query(sql, (err, standResult) => {
+			if (err) res.status(500).send(err);
+
+			if (standResult.length === 0) {
+				return res.status(200).send({ error: true, message: 'Data tidak tersedia!' });
+			} else {
+				return res.status(200).send({ error: false, listStands: standResult });
+			}
+		});
+	},
+
+	/**
+	 * @routes POST public/stand-items
+	 * @description Public get list stand items
+	 * @access Public
+	 */
+	publicGetItemsStand: (req, res) => {
+		// Get Stand Id
+		const profileId = parseInt(req.body.profileId);
+
+		// Set SQL Syntax
+		const sqlItems = `
+      SELECT 
+        si.itemId, 
+        si.itemName, 
+        si.itemPrice, 
+        si.itemDesc, 
+        si.itemPhoto 
+      FROM stand_items si 
+      WHERE si.profileId = ?;`;
+
+		// Database Action
+		db.query(sqlItems, profileId, (err, itemsResult) => {
+			if (err) res.status(500).send(err);
+
+			if (itemsResult.length === 0) {
+				return res.status(200).send({ error: true, message: 'Data tidak tersedia!' });
+			}
+		});
+	}
+};
