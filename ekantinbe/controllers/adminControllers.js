929dad014971b1512e616ca78bf98a78f37b4f79
Bobi Basari <bobibasari@gmail.com>
2020-03-30 10:35:38 +0700

    e-kantin-unpar-backend

--- ekantinbe/controllers/adminControllers.js
+++ ekantinbe/controllers/adminControllers.js
@@ -8,10 +8,10 @@ module.exports = {
 	 * @access Admin
 	 */
 	adminLogin: (req, res) => {
-		// Get email & password
-		const { adminMail, adminPassword } = req.body.data;
+		// Get Email And Password
+		const { adminMail, adminPassword } = req.body; // req.body.data
 
-		// Validation email and password
+		// Validation Email And Password
 		if (adminMail === undefined || adminPassword === undefined) {
 			return res
 				.status(200)
@@ -33,5 +33,98 @@ module.exports = {
 				}
 			});
 		}
+	},
+
+	/**
+	 * @routes POST admin/stand
+	 * @description Admin get stand list
+	 * @access Admin
+	 */
+	adminGetListStand: (req, res) => {
+		// Set SQL Syntax
+		const sqlStand = `
+			SELECT 
+				sp.profileId, 
+				sp.standName, 
+				sp.standContact, 
+				sp.standPhoto
+			FROM stand_profile sp`;
+
+		// Database Action
+		db.query(sqlStand, (err, standResult) => {
+			if (err) res.status(500).send(err);
+
+			if (standResult.length === 0) {
+				return res.status(200).send({ error: true, message: 'Data tidak tersedia!' });
+			} else {
+				return res.status(200).send({ error: false, standResult });
+			}
+		});
+	},
+
+	/**
+	 * @routes POST admin/edit-stand-profile
+	 * @description Admin edit profile stand action
+	 * @access Admin
+	 */
+	adminEditProfileStand: (req, res) => {
+		// Get New Data Stand & Profile Id
+		const { profileId, standName, standContact } = req.body; // req.body.data
+
+		// Validation Data
+		if (
+			standName === undefined ||
+			standName === '' ||
+			standContact === undefined ||
+			standContact === ''
+		) {
+			return res.status(500).send({ error: true, message: 'Data tidak boleh kosong!' });
+		} else {
+			// Set Data
+			const data = {
+				standName,
+				standContact
+			};
+
+			// Set SQL Syntax
+			const sqlEditStand = `UPDATE stand_profile sp SET ? WHERE sp.profileId = ?`;
+
+			// Database Action
+			db.query(sqlEditStand, [data, parseInt(profileId)], (err, editStandResult) => {
+				if (err) res.status(500).send({ error: err });
+
+				if (editStandResult.affectedRows === 0) {
+					return res
+						.status(200)
+						.send({ error: true, message: 'Data tidak berhasil di update!' });
+				} else {
+					return res.status(200).send({ error: false, message: 'Data berhasil di update!' });
+				}
+			});
+		}
+	},
+
+	/**
+	 * @routes POST admin/delete-stand
+	 * @description Admin delete stand action
+	 * @access Admin
+	 */
+	adminDeleteStand: (req, res) => {
+		// Get Profie Id
+		const { profileId } = req.body; // req.body.data
+
+		// Set SQL Syntax
+		const sqlDeleteStand = `DELETE FROM stand_profile sp WHERE sp.profileId = ?`;
+
+		// Database Action
+		db.query(sqlDeleteStand, parseInt(profileId), (err, deleteResult) => {
+			if (err) res.status(500).send({ error: true, err });
+
+			if (deleteResult.affectedRows === 0) {
+				return res.status(200).send({ error: true, message: 'Data tidak di hapus!' });
+			} else {
+				return res.status(200).send({ error: false, message: 'Data berhasil di hapus!' });
+			}
+		});
 	}
 };
--- ekantinbe/routes/adminRoutes.js
+++ ekantinbe/routes/adminRoutes.js
@@ -9,4 +9,25 @@ const { adminControllers } = require('../controllers');
  */
 router.post('/login', adminControllers.adminLogin);
 
+/**
+ * @routes POST admin/stand
+ * @description Admin get stand list
+ * @access Admin
+ */
+router.post('/stand', adminControllers.adminGetListStand);
+
+/**
+ * @routes POST admin/edit-stand-profile
+ * @description Admin edit profile stand action
+ * @access Admin
+ */
+router.post('/edit-stand-profile', adminControllers.adminEditProfileStand);
+
+/**
+ * @routes POST admin/delete-stand
+ * @description Admin delete stand action
+ * @access Admin
+ */
+router.post('/delete-stand', adminControllers.adminDeleteStand);
+
 module.exports = router;
