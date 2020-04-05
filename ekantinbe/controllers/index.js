098de42a485caf8426e61dc88a53f372a5310d91
Bobi Basari <bobibasari@gmail.com>
2020-04-05 20:12:14 +0700

    remove e-kantin-backend

--- ekantinbe/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const adminControllers = require('./adminControllers');
-const publicControllers = require('./publicControllers');
-
-module.exports = {
-	adminControllers,
-	publicControllers
-};
--- ekantinbe/routes/index.js
+++
@@ -1,7 +0,0 @@
-const adminRoutes = require('./adminRoutes');
-const publicRoutes = require('./publicRoutes');
-
-module.exports = {
-	adminRoutes,
-	publicRoutes
-};
