c430c946bb14ec7bba1732998f7e32e76a7900f8
smartoryu <prikenang@gmail.com>
2020-04-07 15:09:04 +0700

    added hashpassword controller

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,82 +1,89 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { adminControllers } = require('../controllers');
+const { adminControllers } = require("../controllers");
+
+/**
+ * @routes POST admin/hashpassword
+ * @description Generate hashing password
+ * @access
+ */
+router.get("/hashpassword", adminControllers.hashpassword);
 
 /**
  * @routes POST admin/register
  * @description Admin register action
  * @access Admin
  */
-router.post('/register', adminControllers.adminRegister);
+router.post("/register", adminControllers.adminRegister);
 
 /**
  * @routes POST admin/login
  * @description Admin login action
  * @access Admin
  */
-router.post('/login', adminControllers.adminLogin);
+router.post("/login", adminControllers.adminLogin);
 
 /**
  * @routes POST admin/stand
  * @description Admin get stand list
  * @access Admin
  */
-router.post('/stand', adminControllers.adminGetListStand);
+router.post("/stand", adminControllers.adminGetListStand);
 
 /**
  * @routes POST admin/add-stand
  * @description Admin create a new stand
  * @access Admin
  */
-router.post('/add-stand', adminControllers.adminAddStand);
+router.post("/add-stand", adminControllers.adminAddStand);
 
 /**
  * @routes POST admin/edit-stand-profile
  * @description Admin edit profile stand action
  * @access Admin
  */
-router.post('/edit-stand-profile', adminControllers.adminEditProfileStand);
+router.post("/edit-stand-profile", adminControllers.adminEditProfileStand);
 
 /**
  * @routes POST admin/edit-stand-photo
  * @description Admin edit photo stand action
  * @access Admin
  */
-router.post('/edit-stand-photo', adminControllers.adminEditPhotoStand);
+router.post("/edit-stand-photo", adminControllers.adminEditPhotoStand);
 
 /**
  * @routes POST admin/delete-stand
  * @description Admin delete stand action
  * @access Admin
  */
-router.post('/delete-stand', adminControllers.adminDeleteStand);
+router.post("/delete-stand", adminControllers.adminDeleteStand);
 
 /**
  * @routes POST admin/menu
  * @description Admin get all menu
  * @access Admin
  */
-router.post('/menu', adminControllers.adminGetStandMenu);
+router.post("/menu", adminControllers.adminGetStandMenu);
 
 /**
  * @routes POST admin/add-menu
  * @description Admin create a new menu
  * @access Admin
  */
-router.post('/add-menu', adminControllers.adminAddStandMenu);
+router.post("/add-menu", adminControllers.adminAddStandMenu);
 
 /**
  * @routes POST admin/edit-menu
  * @description Admin update a menu
  * @access Admin
  */
-router.post('/edit-menu', adminControllers.adminEditStandMenu);
+router.post("/edit-menu", adminControllers.adminEditStandMenu);
 
 /**
  * @routes POST admin/delete-menu
  * @description Admin delete a menu
  * @access Admin
  */
-router.post('/delete-menu', adminControllers.adminDeleteStandMenu);
+router.post("/delete-menu", adminControllers.adminDeleteStandMenu);
 
 module.exports = router;
