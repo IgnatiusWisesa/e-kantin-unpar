14daef01fc790e21d1c16f987b3d8bb4f4d3a77a
Bobi Basari <bobibasari@gmail.com>
2020-04-08 10:02:56 +0700

    custom jwt-auth and jwt

--- ekantinbe/helper/jwt.js
+++ ekantinbe/helper/jwt.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 module.exports = payload => {
+	// Secret Key
 	let privateKEY = fs.readFileSync('./private.key', 'utf8');
 	return jwt.sign(payload, privateKEY, { expiresIn: '6h', algorithm: 'RS256' });
 };
