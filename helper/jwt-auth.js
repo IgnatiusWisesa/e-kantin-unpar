27ed647d1d9ab51f82154aa4402d91a80d315ae0
IgnatiusWisesa <45249515+IgnatiusWisesa@users.noreply.github.com>
2020-04-13 13:47:32 +0700

    proteksi admin

--- helper/jwt-auth.js
+++ helper/jwt-auth.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+require('dotenv').config();
 
 module.exports = {
 	auth: (req, res, next) => {
 		// Get Token
-		const token = req.token;
+		const token = req.token
 
 		// Validation Data
 		if (token) {
 			// Get Public Key
-			let publicKEY = fs.readFileSync('./public.key', 'utf8');
+			let publicKEY = process.env.PUBLIC_KEY.replace(/\\n/g, '\n');
 
 			// JWT Action
 			jwt.verify(token, publicKEY, { expiresIn: '6h', algorithm: ['RS256'] }, (err, decoded) => {
@@ -18,6 +19,7 @@ module.exports = {
 						.status(401)
 						.send({ message: 'User not authorized.', name: 'User not authorized.' });
 				} else {
+					// console.log('decoded', decoded)
 					req.user = decoded;
 					next();
 				}
