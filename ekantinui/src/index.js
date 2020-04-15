8d52e59a9557a91a1d02b72632c0fbf8635885eb
Bobi Basari <bobibasari@gmail.com>
2020-04-15 21:32:24 +0700

    ekantin-unpar

--- ekantinui/src/index.js
+++ ekantinui/src/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import Thunk from "redux-thunk";
-import Reducers from "./redux/reducers";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import Thunk from 'redux-thunk';
+import Reducers from './redux/reducers';
 
-import App from "./App";
-import "./styles/index.css";
+import App from './App';
+import './styles/index.css';
 
 ReactDOM.render(
-  <Provider store={createStore(Reducers, {}, applyMiddleware(Thunk))}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={createStore(Reducers, {}, applyMiddleware(Thunk))}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
