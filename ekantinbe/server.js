f8068aa003059437e2049c420606b9d4477aa3a8
smartoryu <prikenang@gmail.com>
2020-04-16 00:06:03 +0700

    fix cors

--- ekantinbe/server.js
+++ ekantinbe/server.js
@@ -1,23 +1,16 @@
 // Import global module
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const bearerToken = require('express-bearer-token');
-const http = require('http');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bearerToken = require("express-bearer-token");
+const http = require("http");
+require("dotenv").config();
 
 // Set App
 const app = express();
 
 // Set Server
 const server = http.createServer(app);
-
-// Check database connection
-const db = require('./database/db');
-db.connect(err => {
-	if (err) throw err;
-	console.log('Database connected...');
-});
 
 // Parse application / x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,20 +19,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Set cors
-app.use(cors());
+const corsOptions = {
+  origin: "https://unpar-ekantin.firebaseapp.com/",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // Set Token
 app.use(bearerToken());
 
 // Set Static File
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// Check database connection
+const db = require("./database/db");
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected...");
+});
 
 // Set routes
-const { adminRoutes, publicRoutes } = require('./routes');
-app.get('/', (req, res) => res.status(200).send('Server e-kantin Unpar is running...'));
-app.use('/admin', adminRoutes);
-app.use('/public', publicRoutes);
+const { adminRoutes, publicRoutes } = require("./routes");
+app.get("/", (req, res) => res.status(200).send("Server e-kantin Unpar is running..."));
+app.use("/admin", adminRoutes);
+app.use("/public", publicRoutes);
 
 // Set PORT
-const PORT = process.env.PORT || 1919;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
