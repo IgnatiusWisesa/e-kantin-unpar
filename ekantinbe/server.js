d09606ee3444e98f3956b24bb98a2c9063aa9423
smartoryu <prikenang@gmail.com>
2020-04-16 01:34:52 +0700

    tes server

--- ekantinbe/server.js
+++ ekantinbe/server.js
@@ -13,12 +13,12 @@ const app = express();
 const server = http.createServer(app);
 
 // Set cors
-// const corsOptions = {
-//   origin: "https://unpar-ekantin.firebaseapp.com/",
-//   optionsSuccessStatus: 200,
-// };
-
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Parse application / x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +32,23 @@ app.use(bearerToken());
 // Set Static File
 app.use(express.static("public"));
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // Check database connection
 const db = require("./database/db");
 db.connect((err) => {
