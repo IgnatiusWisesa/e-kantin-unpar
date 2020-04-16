a22e7c350ed4cf682107817127312226a52d087d
Bobi Basari <bobibasari@gmail.com>
2020-04-16 19:36:29 +0700

    set cors

--- server.js
+++ server.js
@@ -12,23 +12,23 @@ const app = express();
 // Set cors
 app.use(cors());
 
-app.use(function(req, res, next) {
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
+// app.use(function(req, res, next) {
+// 	// Website you wish to allow to connect
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
 
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	// Request methods you wish to allow
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// 	// Request headers you wish to allow
+// 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	// Set to true if you need the website to include cookies in the requests sent
+// 	// to the API (e.g. in case you use sessions)
+// 	res.setHeader('Access-Control-Allow-Credentials', true);
 
-	// Pass to next layer of middleware
-	next();
-});
+// 	// Pass to next layer of middleware
+// 	next();
+// });
 
 // Set Server
 const server = http.createServer(app);
