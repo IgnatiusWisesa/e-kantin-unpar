01b3aaa1d16f8119b7910435d3ab63a52258fcc0
Bobi Basari <bobibasari@gmail.com>
2020-04-16 20:07:43 +0700

    update cors setting

--- ekantinbe/server.js
+++ ekantinbe/server.js
@@ -28,6 +28,12 @@ app.use(bodyParser.json());
 // Set cors
 app.use(cors());
 
+app.all('/', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Origin', 'X-Requested-With');
+	next();
+});
+
 // Set Token
 app.use(bearerToken());
 
