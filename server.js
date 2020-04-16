45e18e36eb8abcb7aa55b65724dab1e90ebb61c5
Bobi Basari <bobibasari@gmail.com>
2020-04-16 19:10:02 +0700

    set cors

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const server = http.createServer(app);
 
 // Set cors
 app.use(cors());
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+// 	next();
+// });
 
 // Parse application / x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
