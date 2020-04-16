2a134d96cec60ac7f2674c8cbef4a116fa032309
Bobi Basari <bobibasari@gmail.com>
2020-04-16 20:13:53 +0700

    set cors

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(cors());
 app.all('/', function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Origin', 'X-Requested-With');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
 
@@ -23,14 +25,14 @@ app.all('/', function(req, res, next) {
 // 	res.setHeader('Access-Control-Allow-Origin', '*');
 
 // 	// Request methods you wish to allow
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
 // 	// Request headers you wish to allow
 // 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
 // 	// Set to true if you need the website to include cookies in the requests sent
 // 	// to the API (e.g. in case you use sessions)
-// 	res.setHeader('Access-Control-Allow-Credentials', true);
+// res.setHeader('Access-Control-Allow-Credentials', true);
 
 // 	// Pass to next layer of middleware
 // 	next();
