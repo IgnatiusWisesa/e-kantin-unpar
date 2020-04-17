403571a709d9949f20c3451e01c301bf94971d84
Bobi Basari <bobibasari@gmail.com>
2020-04-17 23:36:14 +0700

    config

--- ekantinbe/server.js
+++ ekantinbe/server.js
@@ -1,10 +1,10 @@
 // Import global module
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const bearerToken = require("express-bearer-token");
-const http = require("http");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bearerToken = require('express-bearer-token');
+const http = require('http');
+require('dotenv').config();
 
 // Set App
 const app = express();
@@ -18,26 +18,8 @@ app.all('/', function(req, res, next) {
 	next();
 });
 
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-//   // Request methods you wish to allow
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-
-//   // Request headers you wish to allow
-//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
 // Set Server
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // Parse application / x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -49,20 +31,20 @@ app.use(bodyParser.json());
 app.use(bearerToken());
 
 // Set Static File
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Check database connection
-const db = require("./database/db");
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Database connected...");
+const db = require('./database/db');
+db.connect(err => {
+	if (err) throw err;
+	console.log('MySql connected...');
 });
 
 // Set routes
-const { adminRoutes, publicRoutes } = require("./routes");
-app.get("/", (req, res) => res.status(200).send("Server e-kantin Unpar is running..."));
-app.use("/admin", adminRoutes);
-app.use("/public", publicRoutes);
+const { adminRoutes, publicRoutes } = require('./routes');
+app.get('/', (req, res) => res.status(200).send('Server e-kantin Unpar is running...'));
+app.use('/admin', adminRoutes);
+app.use('/public', publicRoutes);
 
 // Set PORT
 const PORT = process.env.PORT;
