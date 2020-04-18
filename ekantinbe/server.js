7b1ac8de037e37301d8c12e0f7d5f14eb629a457
Bobi Basari <bobibasari@gmail.com>
2020-04-18 19:25:27 +0700

    config cors

--- ekantinbe/server.js
+++ ekantinbe/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 // Add headers
 app.use(function(req, res, next) {
 	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'https://ekantin-unpar.com/*');
+	res.setHeader('Access-Control-Allow-Origin', 'https://ekantin-unpar.com');
 
 	// Request methods you wish to allow
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
